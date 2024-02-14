57a882d4005b99b56d3aa4b6dd724946a135f119
Hao Ruan <ruanhao1116@gmail.com>
2024-02-14 17:21:06 +0800

    create utils.py

--- simple_proxy/__init__.py
+++ simple_proxy/__init__.py
@@ -1,36 +1,36 @@
 import socket
-import platform
-import random
 import http.server
 import ssl
-from functools import wraps, partial
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography import x509
-from cryptography.x509.oid import NameOID
-from cryptography.hazmat.primitives import hashes, serialization
+from functools import wraps
 from py_netty.handler import LoggingChannelHandler
 from py_netty import Bootstrap, ServerBootstrap, EventLoopGroup
-import inspect
-from pathlib import Path
 import traceback
 import sys
-import tempfile
 import os
 import click
-from datetime import datetime, timedelta
+from datetime import datetime
 import logging
 import re
 import codecs
 from collections import defaultdict
 import time
 from attrs import define, field
-import threading
-import itertools
-
+from simple_proxy.utils import (
+    submit_daemon_thread,
+    random_sentence,
+    pretty_duration,
+    format_bytes,
+    pretty_bytes,
+    pretty_speed,
+    from_cwd,
+    getpeername,
+    getsockname,
+    create_temp_key_cert,
+    free_port,
+    set_keepalive
+)
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.WARNING)
 
 
 def _setup_logging(level=logging.INFO):
@@ -48,31 +48,7 @@ __ALL__ = ['run_proxy']
 
 
 _speed_monitor = True
-_counter = itertools.count()
 _stderr = False
-
-
-def _submit_daemon_thread(func, *args, **kwargs) -> threading.Thread:
-    if isinstance(func, partial):
-        func_name = func.func.__name__
-    else:
-        func_name = func.__name__
-
-    def _worker():
-        func(*args, **kwargs)
-
-    t = threading.Thread(target=_worker, name=f'{func_name}-daemon-{next(_counter)}', daemon=True)
-    t.start()
-    return t
-
-
-def _random_sentence():
-    nouns = ("puppy", "car", "rabbit", "girl", "monkey")
-    verbs = ("runs", "hits", "jumps", "drives", "barfs")
-    adv = ("crazily.", "dutifully.", "foolishly.", "merrily.", "occasionally.")
-    return nouns[random.randrange(0, 5)] + ' ' + \
-        verbs[random.randrange(0, 5)] + ' ' + \
-        adv[random.randrange(0, 5)] + '\n'
 
 
 def pstderr(msg):
@@ -84,47 +60,6 @@ def pstderr(msg):
 def pfatal(msg):
     logger.critical(msg)
     exit(1)
-
-
-def _pretty_duration(seconds: int) -> str:
-    TIME_DURATION_UNITS = (
-        ('W', 60 * 60 * 24 * 7),
-        ('D', 60 * 60 * 24),
-        ('H', 60 * 60),
-        ('M', 60),
-        ('S', 1)
-    )
-    if seconds == 0:
-        return '0S'
-    parts = []
-    for unit, div in TIME_DURATION_UNITS:
-        amount, seconds = divmod(int(seconds), div)
-        if amount > 0:
-            parts.append('{}{}'.format(amount, unit))
-    return ', '.join(parts)
-
-
-def _format_bytes(size, scale=1):
-    size = int(size)
-    power = 2**10
-    n = 0
-    power_labels = {0 : 'B', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
-    while size > power:
-        size /= power
-        size = round(size, scale)
-        n += 1
-    if size == int(size):
-        size = int(size)
-    return size, power_labels[n]
-
-
-def _pretty_bytes(size: int) -> str:
-    v, unit = _format_bytes(size)
-    return f"{v} {unit}"
-
-
-def _pretty_speed(speed: int) -> str:
-    return _pretty_bytes(speed) + '/s'
 
 
 @define(slots=True, kw_only=True, order=True)
@@ -152,19 +87,19 @@ class _Client():
     proxy_socket: socket.socket = field(default=None)
 
     def pretty_born_time(self):
-        return _pretty_duration(time.perf_counter() - self.born_time)
+        return pretty_duration(time.perf_counter() - self.born_time)
 
     def pretty_rx_speed(self):
-        return _pretty_speed(self.rbps)
+        return pretty_speed(self.rbps)
 
     def pretty_tx_speed(self):
-        return _pretty_speed(self.wbps)
+        return pretty_speed(self.wbps)
 
     def pretty_rx_total(self):
-        return _pretty_bytes(self.total_read_bytes)
+        return pretty_bytes(self.total_read_bytes)
 
     def pretty_tx_total(self):
-        return _pretty_bytes(self.total_write_bytes)
+        return pretty_bytes(self.total_write_bytes)
 
     def read(self, size):
         self.__class__.global_rx += size
@@ -246,27 +181,6 @@ def sneaky():
     return decorate
 
 
-def _from_cwd(*args):
-    absolute = Path(os.path.join(os.getcwd(), *args))
-    absolute.parent.mkdir(parents=True, exist_ok=True)
-    return absolute
-
-
-def _module_path(mod=None):
-    if not mod:
-        frm = inspect.stack()[1]
-        mod = inspect.getmodule(frm[0])
-    return os.path.dirname(mod.__file__)
-
-
-def _from_module(filename=None):
-    frm = inspect.stack()[1]
-    mod = inspect.getmodule(frm[0])
-    if not filename:
-        return _module_path(mod)
-    return os.path.join(_module_path(mod), filename)
-
-
 @sneaky()
 def _handle(buffer, direction, src, dst, print_content, to_file):
     src_ip, src_port = src.getpeername()
@@ -290,7 +204,7 @@ def _handle(buffer, direction, src, dst, print_content, to_file):
     dst_ip = dst_ip.replace(':', '_')
     filename = ('L' if direction else 'R') + f'_{src_ip}_{src_port}_{dst_ip}_{dst_port}.log'
     if to_file:
-        with _from_cwd('__tcpflow__', filename).open('a') as f:
+        with from_cwd('__tcpflow__', filename).open('a') as f:
             f.write(content)
     if print_content:
         click.secho(content, fg='green' if direction else 'yellow')
@@ -328,39 +242,21 @@ def _clients_check(interval):
             duration = client.pretty_born_time().lower()
             local_socket = client.local_socket
             proxy_socket = client.proxy_socket
-            from_ = _getpeername(local_socket)
-            proxy = _getsockname(proxy_socket)
+            from_ = getpeername(local_socket)
+            proxy = getsockname(proxy_socket)
             pstderr(f"[{count:3}] | {from_:21} | {proxy:21} | rx:{pspeed:10} tx:{pwspeed:10} | cum(rx):{ptotal:10} cum(tx):{pwtotal:10} | {duration}")
             count += 1
         if total:
             average_speed = round(sum([c.rbps for c in clients_snapshot.values()]) / total, 2)
             average_wspeed = round(sum([c.wbps for c in clients_snapshot.values()]) / total, 2)
-            ever_rx, unit_r = _format_bytes(_Client.global_rx)
-            ever_tx, unit_t = _format_bytes(_Client.global_tx)
+            ever_rx, unit_r = format_bytes(_Client.global_rx)
+            ever_tx, unit_t = format_bytes(_Client.global_tx)
             r = f"{ever_rx}{unit_r or 'B'}"
             t = f"{ever_tx}{unit_t or 'B'}"
-            max_rx = _pretty_speed(_Client.max_rx)
-            max_tx = _pretty_speed(_Client.max_tx)
+            max_rx = pretty_speed(_Client.max_rx)
+            max_tx = pretty_speed(_Client.max_tx)
             pstderr(f"Average Rx:{average_speed} bytes/s, Average Tx:{average_wspeed} bytes/s, Ever max Rx:{max_rx}, Ever max Tx:{max_tx}, Total Rx:{r}, Total Tx:{t}")
         time.sleep(interval)
-
-
-def _getpeername(sock: socket.socket) -> str:
-    if not sock:
-        return '?'
-    try:
-        return ':'.join(map(str, sock.getpeername()))
-    except OSError:
-        return '?'
-
-
-def _getsockname(sock: socket.socket) -> str:
-    if not sock:
-        return '?'
-    try:
-        return ':'.join(map(str, sock.getsockname()))
-    except OSError:
-        return '?'
 
 
 class ProxyChannelHandler(LoggingChannelHandler):
@@ -422,7 +318,7 @@ class ProxyChannelHandler(LoggingChannelHandler):
         super().channel_read(ctx, bytebuf)
         if self._client is None:
             if self._white_list and not _check_patterns(self._white_list, ctx.channel().socket().getpeername()[0]):
-                pstderr(f"malicious visitor: {ctx.channel().socket().getpeername()}")
+                pstderr(f"Malicious visitor: {ctx.channel().socket().getpeername()}")
                 self._client_channel(ctx, self._disguise_tls_ip, self._disguise_tls_port)
             else:
                 self._client_channel(ctx, self._remote_host, self._remote_port)
@@ -449,7 +345,7 @@ class MyHttpHandler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(_random_sentence().encode('utf-8'))
+        self.wfile.write(random_sentence().encode('utf-8'))
 
 
 @click.command(short_help="Simple proxy", context_settings=dict(help_option_names=['-h', '--help']))
@@ -475,6 +371,7 @@ def _cli(verbose, **kwargs):
     if verbose:
         _setup_logging(logging.INFO)
         logger.setLevel(logging.DEBUG)
+        logging.getLogger('simple_proxy.utils').setLevel(logging.DEBUG)
     run_proxy(**kwargs)
 
 
@@ -517,7 +414,7 @@ def run_proxy(
 
     if run_mock_tls_server:
         disguise_tls_ip = 'localhost'
-        disguise_tls_port = _free_port()
+        disguise_tls_port = free_port()
         server_address = (disguise_tls_ip, disguise_tls_port)
         kf_mock, cf_mock = create_temp_key_cert(True)
         httpd = http.server.HTTPServer(server_address, MyHttpHandler)
@@ -526,7 +423,7 @@ def run_proxy(
                                        certfile=cf_mock,
                                        keyfile=kf_mock,
                                        ssl_version=ssl.PROTOCOL_TLS)
-        _submit_daemon_thread(httpd.serve_forever)
+        submit_daemon_thread(httpd.serve_forever)
 
     client_eventloop_group = EventLoopGroup(1, 'Client')
     sb = ServerBootstrap(
@@ -549,7 +446,7 @@ def run_proxy(
 
     if speed_monitor:
         import signal
-        _submit_daemon_thread(_clients_check, speed_monitor_interval)
+        submit_daemon_thread(_clients_check, speed_monitor_interval)
 
         def _signal_handler(sig, frame):
             global _speed_monitor
@@ -559,154 +456,6 @@ def run_proxy(
 
         signal.signal(signal.SIGINT, _signal_handler)
     sb.bind(address=local_server, port=local_port).close_future().sync()
-
-
-def generate_private_key():
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-    )
-    return private_key
-
-
-def generate_self_signed_cert(private_key, subject_name, valid_days=365):
-    subject = x509.Name([
-        x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
-        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"CA"),
-        x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
-        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"Simple Proxy"),
-        x509.NameAttribute(NameOID.COMMON_NAME, subject_name),
-    ])
-
-    issuer = subject
-
-    cert = x509.CertificateBuilder().subject_name(
-        subject
-    ).issuer_name(
-        issuer
-    ).public_key(
-        private_key.public_key()
-    ).serial_number(
-        x509.random_serial_number()
-    ).not_valid_before(
-        datetime.utcnow()
-    ).not_valid_after(
-        datetime.utcnow() + timedelta(days=valid_days)
-    ).add_extension(
-        x509.SubjectAlternativeName([
-            x509.DNSName(subject_name),
-        ]),
-        critical=False,
-    ).sign(
-        private_key,
-        algorithm=hashes.SHA256(),
-        backend=default_backend()
-    )
-
-    return cert
-
-
-def save_key_and_cert(private_key, cert, key_file_path, cert_file_path):
-    # Save private key
-    with open(key_file_path, 'wb') as key_file:
-        key_file.write(
-            private_key.private_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PrivateFormat.TraditionalOpenSSL,
-                encryption_algorithm=serialization.NoEncryption()
-            )
-        )
-
-    # Save certificate
-    with open(cert_file_path, 'wb') as cert_file:
-        cert_file.write(
-            cert.public_bytes(serialization.Encoding.PEM)
-        )
-
-
-def create_temp_file(filename):
-    temp_dir = tempfile.mkdtemp()
-    file_path = os.path.join(temp_dir, filename)
-    with open(file_path, 'w'):
-        pass
-    return file_path
-
-
-def create_temp_key_cert(is_for_mock=False):
-    kf_obj = generate_private_key()
-    cf_obj = generate_self_signed_cert(kf_obj, 'localhost')
-    kf = create_temp_file('key.pem')
-    cf = create_temp_file('cert.pem')
-    if is_for_mock:
-        logger.debug(f"[Mock] Generated key and cert: {kf}, {cf}")
-    else:
-        logger.debug(f"Generated key and cert: {kf}, {cf}")
-    save_key_and_cert(kf_obj, cf_obj, kf, cf)
-    return kf, cf
-
-
-def _free_port():
-    temp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    temp_socket.bind(('localhost', 0))
-    _, port = temp_socket.getsockname()
-    temp_socket.close()
-    return port
-
-
-def set_keepalive_linux(sock, after_idle_sec, interval_sec, max_fails):
-    """Set TCP keepalive on an open socket.
-
-    It activates after 1 second (after_idle_sec) of idleness,
-    then sends a keepalive ping once every 3 seconds (interval_sec),
-    and closes the connection after 5 failed ping (max_fails), or 15 seconds
-    """
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
-
-
-def set_keepalive_osx(sock, after_idle_sec, interval_sec, max_fails):
-    """Set TCP keepalive on an open socket.
-
-    sends a keepalive ping once every 3 seconds (interval_sec)
-    """
-    # scraped from /usr/include, not exported by python's socket module
-    TCP_KEEPALIVE = 0x10
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-    sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPALIVE, interval_sec)
-
-
-def set_keepalive_win(sock, after_idle_sec, interval_sec, max_fails):
-    sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, after_idle_sec * 1000, interval_sec * 1000))
-
-
-def set_keepalive(sock, after_idle_sec=60, interval_sec=60, max_fails=5):
-    plat = platform.system()
-    if plat == 'Linux':
-        set_keepalive_linux(sock, after_idle_sec, interval_sec, max_fails)
-    if plat == 'Darwin':
-        set_keepalive_osx(sock, after_idle_sec, interval_sec, max_fails)
-    if plat == 'Windows':
-        set_keepalive_win(sock, after_idle_sec, interval_sec, max_fails)
-
-
-def socket_description(sock):
-    '''[id: 0xd829bade, L:/127.0.0.1:2069 - R:/127.0.0.1:55666]'''
-    sock_id = hex(id(sock))
-    fileno = sock.fileno()
-    s_addr = None
-    try:
-        s_addr, s_port = sock.getsockname()
-        d_addr, d_port = sock.getpeername()
-        return f"[id: {sock_id}, fd: {fileno}, L:/{s_addr}:{s_port} - R:/{d_addr}:{d_port}]"
-        pass
-    except Exception:
-        if s_addr:
-            return f"[id: {sock_id}, fd: {fileno}, LISTENING]"
-        else:
-            return f"[id: {sock_id}, fd: {fileno}, CLOSED]"
 
 
 def _run():
@@ -736,16 +485,16 @@ def _test_Client():
 
 
 def _test_format_bytes():
-    v, u = _format_bytes(1)
+    v, u = format_bytes(1)
     assert v == 1
-    assert u == ''
-    v, u = _format_bytes(1025)
+    assert u == 'B'
+    v, u = format_bytes(1025)
     assert v == 1, v
     assert u == 'K', u
-    v, u = _format_bytes(1025 * 1024)
+    v, u = format_bytes(1025 * 1024)
     assert v == 1, v
     assert u == 'M', u
-    v, u = _format_bytes(60 * 1024 * 1024)
+    v, u = format_bytes(60 * 1024 * 1024)
     assert v == 60, v
     assert u == 'M', u
 
