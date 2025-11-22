a0ab3466b1c164a0ef35ffb05f22d624151c4ff5
Hao Ruan <haoru+bitbucket@cisco.com>
2025-11-22 16:52:34 +0800

    Add ut for ProxyChannelHandler

--- simple_proxy/__init__.py
+++ simple_proxy/__init__.py
@@ -1,3 +1,7 @@
 from .run import run_proxy
+from .handler.shell_channel_handler import ShellChannelHandler
 
-__all__ = ['run_proxy']
+__all__ = [
+    'run_proxy',
+    'ShellChannelHandler',  # for elephant-socks5-client-py
+]
